2f83f3fbf7e118ea15f59dfd674e4f5216160150
Ibne-Shahid <anasbinshahid2004@gmail.com>
2025-11-25 23:56:45 +0600

    env files modified

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = `mongodb+srv://taste-house-db:ybvrM4nognPG6e5Y@cluster0.eqwoetz.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eqwoetz.mongodb.net/?appName=Cluster0`;
 const options = {};
 
 if (!uri) {
